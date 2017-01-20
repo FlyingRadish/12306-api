1b08203426058cc932cb12f4059ebd8a193d405f
houxg <houxg09@foxmail.com>
2017-01-21 04:33:51 +0800

    update package.json

--- init_db.sql
+++ init_db.sql
@@ -1,12 +1,13 @@
 create table station (
   id int auto_increment primary key,
-  station_name varchar(20),
+  name varchar(20),
   telecode varchar(20),
   pinyin_full varchar(60),
   pinyin_short varchar(20)
 );
 
 create table train (
+  id int auto_increment primary key,
   trip_date varchar(20),
   train varchar(20),
   trip_from varchar(20),
